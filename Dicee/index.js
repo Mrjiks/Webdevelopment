ddc772fa43e68edd1a75cc238b64e574d6904e07
Harrison <masterejike50@gmail.com>
2021-12-10 14:48:36 +0100

    js minor edits on the msg when a player wins

--- Dicee/index.js
+++ Dicee/index.js
@@ -5,7 +5,7 @@ var randomimg1Src = "images/" + randomimg1;
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomimg1Src);
 
-// Image2
+// Image2-Player2 features
 var randomNumber2 = Math.floor((Math.random() * 6)) + 1;
 var randomimg2 = "dice" + randomNumber2 + ".png";
 var randomimg2Src = "images/" + randomimg2;
@@ -13,7 +13,7 @@ var randomimg2Src = "images/" + randomimg2;
 var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", randomimg2Src);
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "The winner is Player1 🚩";
+  document.querySelector("h1").innerHTML = "Player1 wins 🚩";
 } else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "🚩Player2 wins";
 } else {
