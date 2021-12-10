2821b9b5e1b34f18837baf4fa8f443c5838ba778
Harrison <masterejike50@gmail.com>
2021-12-10 14:37:05 +0100

    This is my first commit via vsc-added dice game coded

---
+++ DOM/index.js
@@ -0,0 +1,19 @@
+//alert("Hello world");
+//document.querySelector("h1").innerHTML = "Goodnight";
+// document.getElementById("title").innerHTML = "Good bye";
+// document.querySelector("h1").style.color = "red";
+// var parentDiv = document.getElementById("info");
+// var newH1 = document.createElement("h1");
+// var newText = document.createTextNode("This is new h1 heading added by js.");
+// newH1.appendChild(newText);
+// var H1 = parentDiv.firstChild;
+//
+// parentDiv.insertBefore(newH1, H1);
+// var target = parentDiv.childNodes[1];
+// parentDiv.insertBefore(newH1, target.nextSibling);
+//
+//
+// document.querySelector("h1").classList.add("textClass");
+// document.querySelector("h1").classList.add("textClass");
+//
+// var alist = document.getElementById("a").attributes;
